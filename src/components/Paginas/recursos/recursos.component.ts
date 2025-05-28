f467dcdc6a31c2732b4ce1de3fcd006252630b24
lgachig <163140225+lgachig@users.noreply.github.com>
2025-05-27 21:41:55 -0500

    vamossssss

--- src/components/Paginas/recursos/recursos.component.ts
+++ src/components/Paginas/recursos/recursos.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-recursos',
-  imports: [  MatCardModule, MatButtonModule, MatListModule ],
+  imports: [MatCardModule, MatButtonModule, MatListModule, NgIf],
   templateUrl: './recursos.component.html',
   styleUrl: './recursos.component.css'
 })
